b0e39d6c438ca6713528ee700fc33d4e700bc8d9
jluna28 <jluna28@calpoly.edu>
2022-11-04 10:58:22 -0700

    skeleton

---
+++ Asgn03/BoardPanel.java
@@ -0,0 +1,22 @@
+package Asgn03;
+
+import org.w3c.dom.events.EventTarget;
+import org.w3c.dom.events.MouseEvent;
+import org.w3c.dom.views.AbstractView;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class BoardPanel extends JPanel { //implements MouseEvent
+
+
+    public BoardPanel() {
+        super();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+    }
+
+}
