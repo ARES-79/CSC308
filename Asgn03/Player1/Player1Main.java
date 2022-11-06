909387d7ad6b7091ea7f65929553ece72b80b839
archie1802 <jonesarchie37@gmail.com>
2022-11-06 15:15:36 -0800

    add server to each game

--- Asgn03/Player1/Player1Main.java
+++ Asgn03/Player1/Player1Main.java
@@ -8,15 +8,16 @@ import javax.swing.*;
 
 public class Player1Main {
     Client client;
+    Server server;
     Thread serverThread;
     Thread clientThread;
     public Player1Main(){}
 
 
     public synchronized void serverSetup() throws InterruptedException {
-        Server server = new Server(6666);
+        this.server = new Server(6666);
         this.client = new Client("localhost", 4444);
-        this.serverThread = new Thread(server);
+        this.serverThread = new Thread(thisserver);
         this.clientThread = new Thread(this.client);
         this.serverThread.start();
         this.clientThread.start();
@@ -26,7 +27,7 @@ public class Player1Main {
     public static void main(String[] args) throws InterruptedException {
         Player1Main player1 = new Player1Main();
         player1.serverSetup();
-        Game window = new Game(player1.client, "Player1");
+        Game window = new Game(player1.client, player1.server, "Player1");
         window.setSize(1000, 600);
         window.setVisible(true);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
